0cf329c057e55661ae229cc4c4dc2c1957749e3f
Nick Aliferopoulos <aliferopoulos@icloud.com>
2019-01-05 22:22:25 +0200

    Major imporvments.

--- mining.py
+++ mining.py
@@ -2,12 +2,24 @@
 
 # For dataset processing
 import pandas as pd
+import numpy as np
 
 # For system functions
 import sys
 
+# For comparison between models and optimization
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
 # Constants
 DEBUG = True
+THREADS = 10
 COMPETITION = "inf131-data-mining"
 RES_FILE = "../y_pred.csv"
 TRAINING_DATA = "../train.csv"
@@ -41,6 +53,157 @@ def submit_result(data, message):
     api.authenticate()
     api.competition_submit(data, message, COMPETITION)
 
+# Algorithm Comparison
+def compare_algos(X, Y):
+    models = []
+    models.append(('LR', LogisticRegression()))
+    models.append(('LDA', LinearDiscriminantAnalysis()))
+    models.append(('KNN', KNeighborsClassifier()))
+    models.append(('CART', DecisionTreeClassifier(max_depth=32, min_samples_split=40)))
+    models.append(('NB', GaussianNB()))
+    models.append(('SVM', SVC(gamma="scale")))
+    models.append(('RF', RandomForestClassifier(n_jobs=THREADS, random_state=0, max_features=5)))
+
+    results = []
+    names = []
+    scoring = 'accuracy'
+    msgs = ""
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=0)
+        cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)\n" % (name, cv_results.mean(), cv_results.std())
+        msgs += msg
+    dbglog(msgs, "Classification Comparison")    
+
+# Max Depth Comparison
+def max_depth_comparison(X, Y):
+    models = []
+    results = []
+    names = []
+    scoring = 'accuracy'
+    msgs = ""
+    max_depths = np.linspace(1, 64, 64, endpoint=True)
+
+    for max_depth in max_depths:
+        models.append(('RF_' + str(max_depth), RandomForestClassifier(n_jobs=THREADS, random_state=0, max_features=5, max_depth=max_depth)))
+
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=0)
+        cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)\n" % (name, cv_results.mean(), cv_results.std())
+        msgs += msg
+    dbglog(msgs, "max_depth comparison")
+
+# Estimator Count Comparison
+def n_estimators_comparison(X, Y):
+    models = []
+    results = []
+    names = []
+    scoring = 'accuracy'
+    msgs = ""
+    n_estimators = [1, 2, 4, 8, 16, 32, 64, 100, 200]
+
+    for n_estimator in n_estimators:
+        models.append(('RF_' + str(n_estimator), RandomForestClassifier(n_jobs=THREADS, random_state=0, max_features=5, max_depth=13, n_estimators=n_estimator)))
+
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=0)
+        cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)\n" % (name, cv_results.mean(), cv_results.std())
+        msgs += msg
+    dbglog(msgs, "n_estimators comparison")
+
+# Samples Split Comparison
+def min_samples_split_comparison(X, Y):
+    models = []
+    results = []
+    names = []
+    scoring = 'accuracy'
+    msgs = ""
+    #min_samples_splits = np.linspace(0.1, 1.0, 10, endpoint=True) -- Bad results when ceil'ed by the RF implementation of SciKit.
+    min_samples_splits = [2,3,4,5,6,7,8,9,10,11]
+
+    for min_samples_split in min_samples_splits:
+        models.append(('RF_' + str(min_samples_split), RandomForestClassifier(n_jobs=THREADS, random_state=0, max_features=5, max_depth=13, n_estimators=64, min_samples_split=min_samples_split)))
+
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=0)
+        cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)\n" % (name, cv_results.mean(), cv_results.std())
+        msgs += msg
+    dbglog(msgs, "min_samples_split comparison")
+
+# Samples Leaf Comparison
+def min_samples_leaf_comparison(X, Y):
+    models = []
+    results = []
+    names = []
+    scoring = 'accuracy'
+    msgs = ""
+    min_samples_leaves = [2,3,4,5,6,7,8,9,10,11]
+
+    for min_samples_leaf in min_samples_leaves:
+        models.append(('RF_' + str(min_samples_leaf), RandomForestClassifier(n_jobs=THREADS, random_state=0, max_features=5, max_depth=13, n_estimators=64, min_samples_split=6, min_samples_leaf=min_samples_leaf)))
+
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=0)
+        cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)\n" % (name, cv_results.mean(), cv_results.std())
+        msgs += msg
+    dbglog(msgs, "min_samples_leaf comparison")
+
+# Max Features Comparison
+def max_features_comparison(X, Y):
+    models = []
+    results = []
+    names = []
+    scoring = 'accuracy'
+    msgs = ""
+    max_features_numbers = [1,2,3,4,5,6, "auto", "sqrt", "log2"]
+
+    for max_features_number in max_features_numbers:
+        models.append(('RF_' + str(max_features_number), RandomForestClassifier(n_jobs=THREADS, random_state=0, max_depth=13, n_estimators=64, min_samples_split=6, min_samples_leaf=2, max_features=max_features_number)))
+
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=0)
+        cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)\n" % (name, cv_results.mean(), cv_results.std())
+        msgs += msg
+    dbglog(msgs, "max_features comparison")
+
+# Criteria Comparison
+def criteria_comparison(X, Y):
+    models = []
+    results = []
+    names = []
+    scoring = 'accuracy'
+    msgs = ""
+    criteria_choices = ["gini", "entropy"]
+
+    for criteria_choice in criteria_choices:
+        models.append(('RF_' + str(criteria_choice), RandomForestClassifier(n_jobs=THREADS, random_state=0, max_depth=13, n_estimators=64, min_samples_split=6, min_samples_leaf=2, max_features=5, criterion=criteria_choice)))
+
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=0)
+        cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)\n" % (name, cv_results.mean(), cv_results.std())
+        msgs += msg
+    dbglog(msgs, "criteria comparison")
+
 # Import training data
 df_train = pd.read_csv(TRAINING_DATA)
 dbglog(df_train.head(), "Initial Training Data")
@@ -63,6 +226,31 @@ def isWeekend(row):
         return 1
     return 0
 
+import math
+
+def long_lat_to_x(row, isDep=False):
+    if isDep:
+        return math.cos(row["LatitudeDeparture"]) * math.cos(row["LongitudeDeparture"])
+    return math.cos(row["LatitudeArrival"]) * math.cos(row["LongitudeArrival"])
+
+def long_lat_to_y(row, isDep=False):
+    if isDep:
+        return math.cos(row["LatitudeDeparture"]) * math.sin(row["LongitudeDeparture"])
+    return math.cos(row["LatitudeArrival"]) * math.sin(row["LongitudeArrival"])
+
+def long_lat_to_z(row, isDep=False):
+    if isDep:
+        return math.sin(row["LatitudeDeparture"])
+    return math.sin(row["LatitudeArrival"]) 
+
+def tripDistance(row):
+    p1 = np.array((row["DepX"], row["DepY"], row["DepZ"]))
+    p2 = np.array((row["ArX"], row["ArX"], row["ArX"]))
+
+    squared_dist = np.sum(p1**2 + p2**2, axis=0)
+    dist = np.sqrt(squared_dist)
+    return dist
+
 # Calculate Holidays based on USFederalHolidayCalendar
 # from pandas.tseries.holiday import USFederalHolidayCalendar
 # cal = USFederalHolidayCalendar()
@@ -75,6 +263,10 @@ christmas_week_dates = [ christmas + datetime.timedelta(days=i) for i in range(1
 # Summer Vacation - Whole August
 summer_vacation = datetime.datetime.strptime('8-1','%m-%d')
 summer_vacation_week_dates = [ summer_vacation + datetime.timedelta(days=i) for i in range(31) ]
+
+# Fourth of July - 3 July - 5 July - Sudden rise in travelling
+fourth_of_july = datetime.datetime.strptime('7-3','%m-%d')
+fourth_of_july_week_dates = [ summer_vacation + datetime.timedelta(days=i) for i in range(2) ]
 
 # List of busiest US airports
 busy_airports = [
@@ -104,6 +296,9 @@ def isHoliday(row):
     # Calculate "hot" holidays for traveling seperately from generic holidays.
     dt = datetime.datetime.strptime(row["DateOfDeparture"], '%Y-%m-%d')
 
+    #for fourth_of_july_dt in fourth_of_july_week_dates:
+    #    if dt == fourth_of_july_dt.replace(year=dt.year):
+    #        return 3
     for christmas_dt in christmas_week_dates:
         if dt == christmas_dt.replace(year=dt.year):
             return 2
@@ -142,6 +337,32 @@ dbglog(df_train.head(), "Training Data - ArrivalBusy")
 
 # dbglog(df_train.head(), "Training Data - DepartureBusy")
 
+# Apply LongLat To DepXYZ
+df_train['DepX'] = df_train.apply (lambda row: long_lat_to_x (row, isDep=True),axis=1)
+df_train['DepY'] = df_train.apply (lambda row: long_lat_to_y (row, isDep=True),axis=1)
+df_train['DepZ'] = df_train.apply (lambda row: long_lat_to_z (row, isDep=True),axis=1)
+
+df_test['DepX'] = df_test.apply (lambda row: long_lat_to_x (row, isDep=True),axis=1)
+df_test['DepY'] = df_test.apply (lambda row: long_lat_to_y (row, isDep=True),axis=1)
+df_test['DepZ'] = df_test.apply (lambda row: long_lat_to_z (row, isDep=True),axis=1)
+
+# Apply LongLat To ArXYZ
+df_train['ArX'] = df_train.apply (lambda row: long_lat_to_x (row),axis=1)
+df_train['ArY'] = df_train.apply (lambda row: long_lat_to_y (row),axis=1)
+df_train['ArZ'] = df_train.apply (lambda row: long_lat_to_z (row),axis=1)
+
+df_test['ArX'] = df_test.apply (lambda row: long_lat_to_x (row),axis=1)
+df_test['ArY'] = df_test.apply (lambda row: long_lat_to_y (row),axis=1)
+df_test['ArZ'] = df_test.apply (lambda row: long_lat_to_z (row),axis=1)
+
+dbglog(df_train.head(), "Training Data - XYZ")
+
+# Apply Trip Distance
+df_train['Distance'] = df_train.apply (lambda row: tripDistance(row),axis=1)
+df_test['Distance'] = df_test.apply (lambda row: tripDistance(row),axis=1)
+
+dbglog(df_train.head(), "Training Data - Distance")
+
 # Prepare training and data set
 # Drop unused columns
 # 0: DateOfDeparture
@@ -156,8 +377,14 @@ dbglog(df_train.head(), "Training Data - ArrivalBusy")
 # 9: WeeksToDeparture
 # 10: std_wtd
 # 11: PAX (Only in Training Data)
+
 df_train.drop(df_train.columns[[0,2,3,4,6,7,8,9,10,11]], axis=1, inplace=True)
 df_test.drop(df_test.columns[[0,2,3,4,6,7,8,9,10]], axis=1, inplace=True)
+
+df_train.drop(["DepX", "DepY", "DepZ", "ArX", "ArY", "ArZ"], axis=1, inplace=True)
+df_test.drop(["DepX", "DepY", "DepZ", "ArX", "ArY", "ArZ"], axis=1, inplace=True)
+
+dbglog(df_train.head(), "Training Data - Post Drops")
 
 from sklearn.preprocessing import LabelEncoder
 
@@ -168,23 +395,45 @@ df_train['Arrival'] = le.transform(df_train['Arrival'])
 df_test['Departure'] = le.transform(df_test['Departure'])
 df_test['Arrival'] = le.transform(df_test['Arrival'])
 
-import numpy as np
-
-# Logistic Regression Approach
-# from sklearn.linear_model import LogisticRegression
-
-# Random Forest Approach
-from sklearn.ensemble import RandomForestClassifier
-
 X_train = df_train
 X_test = df_test
 y_train = np.ravel(y_train)
 
-# Logistic Regression Approach
-# clf = LogisticRegression()
+# No FutureWarning(s) please. We shall handle this in a better manner in the future. (Oh, the irony)
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-# Random Forest Approach
-clf = RandomForestClassifier(n_jobs=2, random_state=0, max_features=5)
+# Algo comparison
+# compare_algos(X_train, y_train) -- Battle between RFs and CART. Let's make RFs win :P 
+
+# Let's optimize our RF and hope for the best! :D
+
+# max_depth comparison
+# max_depth_comparison(X_train, y_train)
+# Winner is: 13
+
+# n_estimators comparison
+# n_estimators_comparison(X_train, y_train)
+# Winner is: 64 
+
+# min_samples_split comparison
+# min_samples_split_comparison(X_train, y_train)
+# Winner is: 6
+
+# min_samples_leaf comparison
+# min_samples_leaf_comparison(X_train, y_train)
+# Winner is: 2
+
+# max_features comparison
+# max_features_comparison(X_train, y_train)
+# Winner is: 5
+
+# criteria comparison
+# criteria_comparison(X_train, y_train)
+# Winner is: gini
+
+# Based on comparisons above
+clf = RandomForestClassifier(n_jobs=THREADS, random_state=0, max_depth = 13, n_estimators=64, min_samples_split=6, min_samples_leaf=2, max_features=6, criterion="gini") 
 
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
@@ -196,9 +445,6 @@ with open(RES_FILE, 'w', newline='') as csvfile:
     for i in range(y_pred.shape[0]):
         writer.writerow([i, y_pred[i]])
 
-
-
-
-if(input("Do you want to sumbit results now?") == "y"):
+if(input("Do you want to sumbit results now? (y/N): ") == "y"):
     msg = input("Message: ")
     submit_result(RES_FILE, msg)
